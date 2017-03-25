abc190a64da847b40ed352dc81d375a8d709c1d6
SosyalVagon <info@sosyalvagon.com>
2017-03-25 14:49:17 +0200

    Create facebook.js

---
+++ facebook.js
@@ -0,0 +1,9 @@
+<div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/tr_TR/sdk.js#xfbml=1&version=v2.8";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
+<div class="fb-page" data-href="https://www.facebook.com/sosyalvagon" data-tabs="timeline" data-width="300" data-height="80" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true"><blockquote cite="https://www.facebook.com/sosyalvagon" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/sosyalvagon">Sosyal Vagon</a></blockquote></div>
