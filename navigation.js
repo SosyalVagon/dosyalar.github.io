6923bd4e958f9c57f3ae492491ae2d786460b78d
SosyalVagon <info@sosyalvagon.com>
2017-03-25 13:44:31 +0200

    Create navigation.js

---
+++ navigation.js
@@ -0,0 +1,10 @@
+/*<![CDATA[*/
+    var perPage=7;
+    var numPages=6;
+    var firstText ='First';
+    var lastText ='Last';
+    var prevText ='� Previous';
+    var nextText ='Next �';
+    var urlactivepage=location.href;
+    var home_page="/";
+  /*]]>*/
